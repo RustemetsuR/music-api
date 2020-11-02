0f40da7f41cf8ce044099660c5ba9cd5899e771a
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-02 13:22:04 +0600

    Added index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const artists = require("./app/artists");
+const album = require("./app/albums");
+const track = require("./app/tracks");
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const run = async () => {
+    await mongoose.connect("mongodb://localhost/music", { useNewUrlParser: true , useUnifiedTopology: true});
+
+    app.use("/artists", artists());
+    app.use("/albums", album());
+    app.use("/tracks", track());
+    
+    console.log("Connected to mongo DB");
+
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    });
+};
+
+run().catch(console.log);
