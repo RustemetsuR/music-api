00e918189eca18147873bb81329c4ccb96998856
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-02 13:22:37 +0600

    Added models folder with Albums, Artists and Tracks js files

---
+++ models/Tracks.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TrackSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    album: {
+        type: Schema.Types.ObjectId,
+        ref: "Album",
+        required: true,
+    },
+});
+
+const Track = mongoose.model("Track", TrackSchema);
+module.exports = Track;
