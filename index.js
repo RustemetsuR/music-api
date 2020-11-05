548d6b2aac3730aa76c49c764d9fca92e9cd1fd1
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-05 13:06:38 +0600

    Added trackHistory and users routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const artists = require("./app/artists");
 const album = require("./app/albums");
 const track = require("./app/tracks");
+const trackHistory = require("./app/trackHistory");
+const users = require("./app/users");
 const app = express();
 const port = 8000;
 
@@ -12,12 +14,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const run = async () => {
-    await mongoose.connect("mongodb://localhost/music", { useNewUrlParser: true , useUnifiedTopology: true});
+    await mongoose.connect("mongodb://localhost/music", { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
     app.use("/artists", artists());
     app.use("/albums", album());
     app.use("/tracks", track());
-    
+    app.use("/users", users);
+    app.use("/trackHistory", trackHistory);
     console.log("Connected to mongo DB");
 
     app.listen(port, () => {
